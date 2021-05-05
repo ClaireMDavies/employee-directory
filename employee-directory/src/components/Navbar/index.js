32eb83754b12ef218ea3a7b20477ef62e9a1e775
Claire Davies <claire.davies@junitech.com>
2021-05-05 15:21:48 +0100

    adding navbar component

---
+++ employee-directory/src/components/Navbar/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./style.css";
+
+function Navbar() {
+    return ( 
+    <nav class="navbar navbar-dark bg-dark">
+  <div class="container-fluid">
+    <span class="navbar-text">
+      Employee Directory
+    </span>
+  </div>
+</nav>
+);
+}
+
+
+export default Navbar;
