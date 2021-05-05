d2c863c17d263bc4a7384b66be778a430499fcbb
Claire Davies <claire.davies@junitech.com>
2021-05-05 16:30:06 +0100

    hard code of table Body component

--- employee-directory/src/App.js
+++ employee-directory/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import TableHeader from "./components/TableHeader";
+import TableBody from "./components/TableBody";
 import './App.css';
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
         </div>
         <table class="table table-striped">
             <TableHeader/>
+            <TableBody/>
         </table>
     </Router>
     
