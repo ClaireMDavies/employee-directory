9f5221d5f5a9476ba011e7b6ed20375f3793a07d
Claire Davies <claire.davies@junitech.com>
2021-05-07 18:11:49 +0100

    moving vode to directory page, and creating API

--- employee-directory/src/App.js
+++ employee-directory/src/App.js
@@ -1,21 +1,20 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
-import TableHeader from "./components/TableHeader";
-import TableBody from "./components/TableBody";
+import Directory from "./pages/Directory";
 import './App.css';
 
 function App() {
   return (
-    <Router>
-      <div>
+    
+      <div className = "App">
         <Navbar />
+        <div className = "content">
+            <Directory/>
         </div>
-        <table class="table table-striped">
-            <TableHeader/>
-            <TableBody/>
-        </table>
-    </Router>
+        
+  
+        </div>
+        
     
   );
 }
---
+++ employee-directory/src/pages/Directory.js
@@ -0,0 +1,16 @@
+import React from "react";
+import TableHeader from "../components/TableHeader";
+import TableBody from "../components/TableBody";
+
+function Directory() {
+    return (
+        <div>
+            <table class="table table-striped">
+                    <TableHeader />
+                    <TableBody />
+            </table>
+        </div>
+    );
+}
+
+export default Directory;
